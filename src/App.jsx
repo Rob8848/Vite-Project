297d3f714a86acb6534f3a419c7be258a50ab05e
Rob8848 <bhattaraipratik372@gmail.com>
2022-12-28 19:28:24 -0600

    Made changes on APP.JSX for counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,8 @@
-// useReducer: simple Counter
-// 💯 traditional dispatch object with a type and switch statement
-
-import * as React from "react";
-
-function countReducer(state, action) {
-  const { type, step } = action;
-  switch (type) {
-    case "increment": {
-      return {
-        ...state,
-        count: state.count + step,
-      };
-    }
-    default: {
-      throw new Error(`Unsupported action type: ${type}`);
-    }
-  }
-}
+import { useState } from "react";
 
 function Counter({ initialCount = 0, step = 1 }) {
-  const [state, dispatch] = React.useReducer(countReducer, {
-    count: initialCount,
-  });
-  const { count } = state;
-  const increment = () => dispatch({ type: "increment", step });
+  const [count, setCount] = useState(initialCount);
+  const increment = () => setCount(count + step);
   return <button onClick={increment}>{count}</button>;
 }
 
